5e1cb5b9b5843bcd34803886b6aa148b7e42e580
EmanuelaMollova <emanuela.mollova@gmail.com>
2014-03-27 20:59:26 +0200

    Fix bugs

--- maillist_database_adapter.py
+++ maillist_database_adapter.py
@@ -6,7 +6,7 @@ class MailListDataBaseAdapter():
     def __init__(self, db_name, mail_list=None):
         self.db_name = db_name
         self.mail_list = mail_list
-        self._connection = self._create_connection()
+        self._connection = self._create_connection(self.db_name)
 
     def _create_connection(self, db_name):
         return sqlite3.connect(db_name)
@@ -20,10 +20,10 @@ class MailListDataBaseAdapter():
     def _close_connection(self):
         self._conncetion.close()
 
-    def create_table(self, cursor):
-        cursor.execute(''' CREATE TABLE maillist
-            (id int, name text)''')
-        cursor.execute(''' CREATE TABLE subscribers
-            (id int, name text, email text)''')
-        cursor.execute(''' CREATE TABLE m_to_s
-            (list_id int, subscriber_id int)''')
+    def create_tables(self, cursor):
+        cursor.execute(''' CREATE TABLE IF NOT EXISTS maillist
+            (maillist_id INTEGER PRIMARY KEY, name text)''')
+        cursor.execute(''' CREATE TABLE IF NOT EXISTS subscribers
+            (subscriber_id INTEGER PRIMARY KEY, name text, email text)''')
+        cursor.execute(''' CREATE TABLE IF NOT EXISTS maillists_to_subscribers
+            (maillist_id int, subscriber_id int)''')
